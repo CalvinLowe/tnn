0e601c639714a58d7e3ee83587fa30e867cac22d
Calvin Lowe <calvin@calvinlowe.com>
2017-08-02 11:25:10 +1000

    Mailchimp integration, installed WooCommerce

--- template-parts/front-page/mailchimp-opt-in.php
+++ template-parts/front-page/mailchimp-opt-in.php
@@ -8,9 +8,28 @@
  */
 
 ?>
-<!-- to be sanitised - will be connected to the mailchimp -->
-<h2 class="section-title"><?php esc_html_e( 'For weekly recipes, articles, performance tips and giveaways', 'tnn' ); ?></h2>
-<form class="mailchimp-opt-in-form">
-  <input class="mailchimp-input mailchimp-opt-in-name-input" type="text" placeholder="First Name">
-  <input class="mailchimp-input mailchimp-opt-in-email-input" type="email" placeholder="Your Email Address">
-</form>
+<!-- Begin MailChimp Signup Form -->
+<div id="mc_embed_signup">
+  <form action="//thenaturalnutritionist.us7.list-manage.com/subscribe/post?u=62607cf7c4829eff92f2773b1&amp;id=2f8f17bcdf" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate mailchimp-opt-in-form" target="_blank" novalidate>
+    <div id="mc_embed_signup_scroll">
+      <h2 class="section-title"><?php esc_html_e( 'For weekly recipes, articles, performance tips and giveaways', 'tnn' ); ?></h2>
+      <div class="form-wrapper">
+        <div class="mc-field-group form-element">
+          <label for="mce-FNAME">First Name </label>
+          <input type="text" value="" name="FNAME" class="mailchimp-input mailchimp-opt-in-name-input" id="mce-FNAME" placeholder="First Name">
+        </div>
+        <div class="mc-field-group form-element">
+          <label for="mce-EMAIL">Email Address  </label>
+          <input type="email" value="" name="EMAIL" class="required email mailchimp-input mailchimp-opt-in-name-input" id="mce-EMAIL" placeholder="Your Email Address">
+        </div>
+        <div id="mce-responses" class="clear">
+          <div class="response" id="mce-error-response" style="display:none"></div>
+          <div class="response" id="mce-success-response" style="display:none"></div>
+        </div>    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+        <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_62607cf7c4829eff92f2773b1_2f8f17bcdf" tabindex="-1" value=""></div>
+        <div class="clear form-element"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+      </div>
+    </div>
+  </form>
+</div>
+<!--End mc_embed_signup-->
