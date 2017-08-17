be5f0c128c3cd645ccaff0dc29a5453378cd1e56
Calvin Lowe <calvin@calvinlowe.com>
2017-08-17 15:19:08 +1000

    Book your consultation
    + Removed book your consultation section from small screens
    - Need to add them as a button to the mobile menu

--- template-parts/front-page/book-your-consultation.php
+++ template-parts/front-page/book-your-consultation.php
@@ -9,5 +9,5 @@
 
 ?>
 
-<h2 class="book-your-consultation-title section-title-large"><span><?php esc_html_e( 'Book your consultation', 'tnn' ); ?></span><span><?php esc_html_e( 'with TNN ', 'tnn' ); ?> <a class="book-your-consultation-link" href="#"><?php esc_html_e( 'here', 'tnn' ); ?></a></span></h2>
+<h2 class="book-your-consultation-title"><span><?php esc_html_e( 'Book your consultation', 'tnn' ); ?></span><span><?php esc_html_e( 'with TNN ', 'tnn' ); ?> <a class="book-your-consultation-link" href="#"><?php esc_html_e( 'here', 'tnn' ); ?></a></span></h2>
 <img alt="Steph Lowe" src="http://the-natural-nutritionist.dev/wp-content/themes/tnn/img/head_cutout_4.png" >
