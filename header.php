cfe1126288db794054588c6f84da36cffdba305d
Calvin Lowe <calvin@calvinlowe.com>
2017-08-21 14:21:26 +1000

    Hidden navigation item
    + Added a navigation item only to mobile

--- header.php
+++ header.php
@@ -48,7 +48,7 @@
 
 		<nav id="site-navigation" class="main-navigation">
 			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Menu ', 'tnn' ); ?><i class="fa fa-bars" aria-hidden="true"></i>
-</button>
+			</button>
 			<?php
 				wp_nav_menu( array(
 					'theme_location' => 'menu-1',
