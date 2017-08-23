48ed2879b560852689dee2fcfa66cb534714499e
Calvin Lowe <calvin@calvinlowe.com>
2017-08-23 11:06:15 +1000

    Added slides using ACF

--- template-parts/front-page/slider-banner.php
+++ template-parts/front-page/slider-banner.php
@@ -10,7 +10,7 @@
 ?>
 <div class="banner-slider">
   <div class="banner-slider-cell">
-    <a href="<?php get_field('banner_slider_img_link_1') ?>">
+    <a href="<?php the_field('banner_slide_link_1') ?>">
       <?php
       $sliderImage1 = get_field('banner_slider_img_1');
       if( !empty($sliderImage1) ): ?>
@@ -19,7 +19,7 @@
     </a>
   </div> <!-- Slide 1 -->
   <div class="banner-slider-cell">
-    <a href="<?php get_field('banner_slider_img_link_2') ?>">
+    <a href="<?php the_field('banner_slide_link_2') ?>">
     <?php
     $sliderImage2 = get_field('banner_slider_img_2');
     if( !empty($sliderImage2) ): ?>
@@ -28,7 +28,7 @@
     </a>
   </div> <!-- Slide 2 -->
   <div class="banner-slider-cell">
-    <a href="<?php get_field('banner_slider_img_link_3') ?>">
+    <a href="<?php the_field('banner_slide_link_3') ?>">
       <?php
         $sliderImage3 = get_field('banner_slider_img_3');
         if( !empty($sliderImage3) ): ?>
@@ -40,19 +40,31 @@
 
 <!-- TODO Make mobile sliders into ACF -->
 <div class="banner-slider-mb">
-  <div class="banner-slider-cell-mb">
-    <a href="#">
-      <img class="slider-img" src="/wp-content/themes/tnn/img/sliders/TNN_BannerImages_600x600_1.jpg" alt="" />
-    </a>
-  </div> <!-- Slide 1 -->
-  <div class="banner-slider-cell-mb">
-    <a href="#">
-      <img class="slider-img" src="/wp-content/themes/tnn/img/sliders/TNN_BannerImages_600x600_2.jpg" alt="" />
-    </a>
-  </div> <!-- Slide 2 -->
-  <div class="banner-slider-cell-mb">
-    <a href="#">
-      <img class="slider-img" src="/wp-content/themes/tnn/img/sliders/TNN_BannerImages_600x600_3.jpg" alt="" />
-    </a>
-  </div> <!-- Slide 3 -->
-</div> <!-- .banner-slider -->
+  <?php
+  $sliderImage1mb = get_field('banner_slide_1_mb');
+  if( !empty($sliderImage1mb) ): ?>
+    <div class="banner-slider-cell-mb">
+      <a href="<?php the_field('banner_slide_link_1') ?>">
+        <img class="slider-img" src="<?php echo esc_html_e($sliderImage1mb['url']); ?>" alt="<?php echo esc_html_e($sliderImage1mb['alt']); ?>" />
+      </a>
+    </div>
+  <?php endif; ?><!-- Slide 1 -->
+  <?php
+  $sliderImage2mb = get_field('banner_slide_2_mb');
+  if( !empty($sliderImage2mb) ): ?>
+    <div class="banner-slider-cell-mb">
+      <a href="<?php the_field('banner_slide_link_2') ?>">
+        <img class="slider-img" src="<?php echo esc_html_e($sliderImage2mb['url']); ?>" alt="<?php echo esc_html_e($sliderImage2mb['alt']); ?>" />
+      </a>
+    </div>
+  <?php endif; ?><!-- Slide 2 -->
+  <?php
+  $sliderImage3mb = get_field('banner_slide_3_mb');
+  if( !empty($sliderImage3mb) ): ?>
+    <div class="banner-slider-cell-mb">
+      <a href="<?php the_field('banner_slide_link_3') ?>">
+        <img class="slider-img" src="<?php echo esc_html_e($sliderImage3mb['url']); ?>" alt="<?php echo esc_html_e($sliderImage3mb['alt']); ?>" />
+      </a>
+    </div>
+  <?php endif; ?><!-- Slide 3 -->
+</div> <!-- .banner-slider-mb -->
