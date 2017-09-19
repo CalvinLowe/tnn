803f5512ac37661f384127c7d8a8e2eafdd837d9
Calvin Lowe <calvin@calvinlowe.com>
2017-09-19 11:56:44 +1000

    The fiddly bits
    Browser testing

--- template-parts/blog/footer-social.php
+++ template-parts/blog/footer-social.php
@@ -15,12 +15,8 @@
         <?php $link = get_permalink(); ?>
         <li><a href="<?php esc_html_e('https://www.facebook.com/sharer.php?' . $link); ?>"><i class="fa fa-facebook fa-2x" aria-hidden="true"></i></a></li>
         <li><a href="<?php esc_html_e('https://twitter.com/share?url=' . $link . '&text=' . get_the_title() . '&via=@natnutritionist'); ?>"><i class="fa fa-twitter fa-2x" aria-hidden="true"></i></a></li>
-        <li><a href="#"><i class="fa fa-pinterest fa-2x" aria-hidden="true"></i></a></li>
+        <!--<li><a href="#"><i class="fa fa-pinterest fa-2x" aria-hidden="true"></i></a></li>-->
         <li><a href="<?php esc_html_e('mailto:?subject=' . get_the_title() . '&body=' . get_the_excerpt() . '%0A%0A' . $link); ?>"><i class="fa fa-envelope-o fa-2x" aria-hidden="true"></i></a></li>
     </ul>
     </div><!-- #sidebar-social-icons -->
-    <div class="footer-subscribe">
-        <h3><?php echo esc_html_e('Subscribe to the newsletter'); ?></h3>
-        <a class="button" href="#subscribe"><?php echo esc_html_e('Subscribe'); ?></a>
-    </div>
 </div>
