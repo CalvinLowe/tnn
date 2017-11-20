c2377d7fb8a7b36e1f3ae5c23e7b4eb301d7b5ae
Calvin Lowe <calvin@calvinlowe.com>
2017-11-20 18:39:00 +1000

    Removed unprintable characters

--- header.php
+++ header.php
@@ -14,7 +14,7 @@
 <html <?php language_attributes(); ?>>
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
-<meta http-equiv="x-ua-compatible" content="ie=edge"> <!-- † -->
+<meta http-equiv="x-ua-compatible" content="ie=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 
