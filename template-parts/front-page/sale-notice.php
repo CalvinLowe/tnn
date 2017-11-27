bf3c7a37211b5d2c08177542b0d42067b0fc082e
Calvin Lowe <calvin@calvinlowe.com>
2017-11-27 13:30:39 +1000

    Add sales notice button

--- template-parts/front-page/sale-notice.php
+++ template-parts/front-page/sale-notice.php
@@ -10,5 +10,6 @@
 ?>
 <div class="sales-notice">
     <h2 class="sales-notice-title section-title-large"><?php esc_html_e('Sale On Now', 'tnn'); ?></h2>
+    <a href="<?php echo esc_url('/store/books/the-real-food-athlete-gift-package/'); ?>" class="button"><?php esc_html_e('Buy one get one free', 'tnn'); ?></a>
     <p class="sales-notice-paragraph"><?php esc_html_e('Buy a copy of The Real Food Athlete and receive a complementary copy for a friend', 'tnn'); ?></p>
 </div>
