a3535d11c6556e81105894f6c954afe420b526d9
Calvin Lowe <calvin@calvinlowe.com>
2018-01-16 14:22:33 +1000

    Author sidebar/footer for posts
    - Separated variables and function calls
    - Added lniks and descriptions to the sidebar and footer
    - Increased avatar size on larger devices

--- template-parts/blog/footer-author.php
+++ template-parts/blog/footer-author.php
@@ -8,12 +8,21 @@
  */
 
 ?>
-
+<?php
+	// Author meta variables
+	$postID = get_the_ID();
+	$authorID = get_the_author_meta( 'ID' );
+	$authorName = get_the_author();
+	$authorFirstName = get_the_author_meta ( 'first_name' );
+	$desc = get_the_author_meta( 'description' );
+	$url = "https://thenaturalnutritionist.com.au/about#$authorFirstName";
+	$time = get_the_time('F j, Y');
+?>
 <div id="footer-author-info" class="blog-footer author">
-	<?php echo get_avatar( get_the_author_meta( 'ID' ) , 100 ); ?>
-	<h3><?php the_author(); ?></h3>
-	<p><?php the_author_meta('description'); ?></p>
-	<p><?php the_time('F j, Y'); ?></p>
+	<?php echo get_avatar( $authorID , 100 ); ?>
+	<a href="<?php echo esc_url( $url ); ?>"><h3><?php echo $authorName ?></h3></a>
+	<p><?php echo $desc; ?></p>
+	<p><?php echo $time ?></p>
 	<div class="tag-wrap">
 		<?php the_tags(); ?>
 	</div>
