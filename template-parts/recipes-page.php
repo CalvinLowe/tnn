0d56519d2c8873b9210ce1bcda5a16d36ce2c03e
Calvin Lowe <calvin@calvinlowe.com>
2018-02-06 11:20:46 +1000

    Creating recipes page grid

--- template-parts/recipes-page.php
+++ template-parts/recipes-page.php
@@ -9,11 +9,45 @@
 
 ?>
 <div class="recipes-grid">
-    <figure>
-        <!--<a href="#">-->
-            <h3>Mains</h3>
-            <img src="/wp-content/uploads/2016/12/TNN-Recipes-Breakfast.jpg" alt="TNN Breakfast Recipes - Egg Free Breakfast Hash">
-            <figcaption>Blah blah blah</figcaption>
-        <!--</a>-->
-    </figure>
+        
+        <a class="recipes-grid-link recipes-grid-area-1" href="#">
+            <h2 class="recipes-grid-title">Mains</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Mains.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-2" href="#">
+            <h2 class="recipes-grid-title">Breakfast</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Breakfast.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-3" href="#">
+            <h2 class="recipes-grid-title">Smoothies</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Smoothies.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-4" href="#">
+            <h2 class="recipes-grid-title">Bread</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Bread.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-5" href="#">
+            <h2 class="recipes-grid-title">Snacks</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Snacks.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-6" href="#">
+            <h2 class="recipes-grid-title">Sweet Treats</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Sweet-Treats.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-7" href="#">
+            <h2 class="recipes-grid-title">Vegetarian</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-Vegetarian.jpg" alt="#">
+        </a>
+
+        <a class="recipes-grid-link recipes-grid-area-8" href="#">
+            <h2 class="recipes-grid-title">LCHF</h3>
+            <img class="recipes-grid-image" src="/wp-content/uploads/2016/12/TNN-Recipes-LCHF.jpg" alt="">
+        </a>
+    </div>
 </div>
