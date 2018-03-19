400fadffac7e302b4e4f93b0587ec31e5f70b658
Calvin Lowe <calvin@calvinlowe.com>
2018-03-19 14:48:59 +1000

    Front page template - product adverts
    - Updated product advert css to enabled different height adverts to be added
    - Improved template part logic to hide html if nothing is added to ACF
    - Added space for 3rd advert in template

--- template-parts/front-page/advertising-products.php
+++ template-parts/front-page/advertising-products.php
@@ -8,30 +8,42 @@
  */
 
 ?>
+<?php
+/* Variables */
+$image1 = get_field('product_img_1');
+$image2 = get_field('product_img_2');
+$image3 = get_field('product_img_3');
+$link1 = get_field('product_link_1');
+$link2 = get_field('product_link_2');
+$link3 = get_field('product_link_3');
+
+if( !empty($image1) && !empty($image2) && !empty($image3) ): ?>
 <div class="front-page-product-advertising">
+<?php
+  if( !empty($image1) && !empty($link1) ): ?>
   <figure class="product-advert">
-    <a href="<?php the_field('product_1_link'); ?>">
-    <?php
-    $image1 = get_field('product_1_img');
-
-    if( !empty($image1) ): ?>
-
-      <img src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
-
-    <?php endif; ?>
+    <a href="<?php the_field('product_link_1'); ?>">
+        <img src="<?php echo esc_url($image1['url']); ?>" alt="<?php echo esc_attr($image1['alt']); ?>" />
     </a>
   </figure>
+<?php endif; ?>
 
+<?php
+  if( !empty($image2) && !empty($link2) ): ?>
   <figure class="product-advert">
-    <a href="<?php the_field('product_2_link'); ?>">
-    <?php
-    $image2 = get_field('product_2_img');
-
-    if( !empty($image2) ): ?>
-
-      <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
-
-    <?php endif; ?>
+    <a href="<?php the_field('product_link_2'); ?>">
+        <img src="<?php echo esc_url($image2['url']); ?>" alt="<?php echo esc_attr($image2['alt']); ?>" />
     </a>
   </figure>
+<?php endif; ?>
+
+<?php
+  if( !empty($image3) & !empty($link3) ): ?>
+  <figure class="product-advert">
+    <a href="<?php the_field('product_link_3'); ?>">
+        <img src="<?php echo esc_url($image3['url']); ?>" alt="<?php echo esc_attr($image3['alt']); ?>" />
+    </a>
+  </figure>
+<?php endif; ?>
 </div>
+<?php endif; ?>
