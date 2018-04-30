2ace84ddc4cc1a395e49224f0a59f879bfc691be
Calvin Lowe <calvin.lowe@uqconnect.edu.au>
2018-04-30 12:26:20 +1000

    Added amplitude.js HTML5 audio player

---
+++ inc/podcast-audio.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Enque Amplitude JS on podcast page.
+ *
+ * @package TNN
+ */
+
+ //page-id-6037
+
+
+ function podcast_audio_head() {
+	if(is_page( 'podcast' )){
+        echo '<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/amplitudejs@3.2.3/dist/amplitude.js"></script>';
+	}
+}
+add_action('wp_head', 'podcast_audio_head');
